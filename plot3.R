bad205da08cea1bf7f5d850d866b1e5429414fd1
JenSteele <jensteele03@gmail.com>
2015-07-13 10:13:02 +1200

    Updated to improve code and correct graph 2
    Graph 2 was incorrect, and code was messy (messier)

--- plot3.R
+++ plot3.R
@@ -1,22 +1,20 @@
 data <- read.table("C:/Users/jste644/Documents/R/Exploratory_data_analysis/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
 dat <- subset(data,Date=="1/2/2007" | Date=="2/2/2007")
 
-dat$Date <- as.Date(dat$Date,"%d/%m/%Y")
+attach(dat)
+Date <- as.Date(Date,"%d/%m/%Y")
+
 library(lubridate)
-dat$date_time <- paste(dat$Date,dat$Time,sep=" ")
-dat$date_time <- ymd_hms(dat$date_time)
-dat$day <- wday(dat$date_time, label = TRUE, abbr = FALSE)
-xrange <- range(dat$date_time)
-yrange <- range(dat$Sub_metering_1) 
-days <- c("Thursday","Friday","Saturday")
+date_time <- paste(Date,Time,sep=" ")
+date_time <- ymd_hms(date_time)
+xrange <- range(date_time)
+yrange <- range(Sub_metering_1) 
 
 png("C:/Users/jste644/Documents/R/Exploratory_data_analysis/plot3.png",width=480,height=480)
-plot(dat$date_time, dat$Sub_metering_1,axes=T, ylim=yrange, typ='l', ann=T,ylab="Energy sub metering",xlab="")
-lines(dat$date_time,dat$Sub_metering_2,lwd=1,type="l",col="red")
-lines(dat$date_time,dat$Sub_metering_3,lwd=1,type="l",col="blue")
 
-#axis(1,at=c("2007-02-01 00:00:00","2007-02-02 00:00:00","2007-02-02 23:59:00"),
-#    labels=days, lwd=2, lwd.ticks=2)
+plot(date_time, Sub_metering_1,axes=T, ylim=yrange, typ='l', ann=T,ylab="Energy sub metering",xlab="")
+  lines(date_time,Sub_metering_2,lwd=1,type="l",col="red")
+  lines(date_time,Sub_metering_3,lwd=1,type="l",col="blue")
+  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=c(2.5,2.5,2.5),col=c("black","blue","red"),bty="n")
 
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=c(2.5,2.5,2.5),col=c("black","blue","red"),bty="n")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -2,20 +2,25 @@ data <- read.table("C:/Users/jste644/Documents/R/Exploratory_data_analysis/house
 dat <- subset(data,Date=="1/2/2007" | Date=="2/2/2007")
 
 dat$Date <- as.Date(dat$Date,"%d/%m/%Y")
+
 library(lubridate)
 dat$date_time <- paste(dat$Date,dat$Time,sep=" ")
 dat$date_time <- ymd_hms(dat$date_time)
-dat$day <- wday(dat$date_time, label = TRUE, abbr = FALSE)
-Global <- dat$Global_active_power
-yrange <- range(dat$Global_active_power) 
 
 png("C:/Users/jste644/Documents/R/Exploratory_data_analysis/plot4.png",width=480,height=480)
+attach(dat)
 par(mfrow=c(2,2))
-plot(dat$date_time,dat$Global_active_power, axes=T, ylim=yrange, typ='l', ann=T,ylab="Global Active Power (kilowatts)",xlab="")
-plot(dat$date_time,dat$Voltage, axes=T, ylim=range(dat$Voltage), typ='l', ann=T,ylab="Voltage",xlab="datetime")
-plot(dat$date_time, dat$Sub_metering_1,axes=T, ylim=range(dat$Sub_metering_1), typ='l', ann=T,ylab="Energy sub metering",xlab="")
-lines(dat$date_time,dat$Sub_metering_2,lwd=1,type="l",col="red")
-lines(dat$date_time,dat$Sub_metering_3,lwd=1,type="l",col="blue")
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=c(2.5,2.5,2.5),col=c("black","blue","red"),bty="n")
-plot(dat$date_time,dat$Global_reactive_power, axes=T, ylim=range(dat$Global_reactive_power), typ='l', ann=T,ylab="Global_reactive_power",xlab="datetime")
+
+plot(date_time,Global_active_power, axes=T, ylim=range(Global_active_power), typ='l', ann=T,ylab="Global Active Power (kilowatts)",xlab="")
+
+plot(date_time,Voltage, axes=T, ylim=range(Voltage), typ='l', ann=T,ylab="Voltage",xlab="datetime")
+
+plot(date_time, Sub_metering_1,axes=T, ylim=range(Sub_metering_1), typ='l', ann=T,ylab="Energy sub metering",xlab="")
+  lines(date_time,Sub_metering_2,lwd=1,type="l",col="red")
+  lines(date_time,Sub_metering_3,lwd=1,type="l",col="blue")
+  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=c(2.5,2.5,2.5),col=c("black","blue","red"),bty="n")
+
+plot(date_time,Global_reactive_power, axes=T, ylim=range(Global_reactive_power), typ='l', ann=T,ylab="Global_reactive_power",xlab="datetime")
+
 dev.off()
+
