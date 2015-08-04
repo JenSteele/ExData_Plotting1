6830d94bbc47cf5f757d03fcd41f645b7998dc9b
JenSteele <jensteele03@gmail.com>
2015-08-04 12:44:44 +1200

    Updated code for course project 1
    Corrected and updated code

--- plot4.R
+++ plot4.R
@@ -1,3 +1,6 @@
+#Exploratory Data Analysis Course Project 1
+#Plot 4
+
 data <- read.table("C:/Users/jste644/Documents/R/Exploratory_data_analysis/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
 dat <- subset(data,Date=="1/2/2007" | Date=="2/2/2007")
 
